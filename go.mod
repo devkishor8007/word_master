990d3da0601d5b0e3c718015adbb946fa700d197
devkishor <kishor8007kc@gmail.com>
2023-10-03 23:42:55 +0545

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/devkishor8007/word_master
+
+go 1.21.1
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+    "net/http"
+	"log"
+
+	"github.com/joho/godotenv"
+	"os"
+    "github.com/gorilla/mux"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	router := mux.NewRouter()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, World!")
+	}
+	
+	router.HandleFunc("/", handler)
+
+	server := &http.Server{
+		Addr:    ":"+ port,
+		Handler: router,
+	}
+
+	fmt.Println("Server is running on :"+port)
+	log.Fatal(server.ListenAndServe())
+}
