88594d3ab46a59eae78db7f88eb1e7375ab7e80d
devkishor <kishor8007kc@gmail.com>
2023-10-05 00:15:15 +0545

    refactor(database): Use environment variable for postgres connection

--- src/database/db.go
+++ src/database/db.go
@@ -6,13 +6,25 @@ import (
     "log"
     "github.com/devkishor8007/word_master/src/models"
     "fmt"
+	"os"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-    dsn := "host=localhost user=postgres password=postba123@ dbname=word_master sslmode=disable"
-    
+	env_err := godotenv.Load()
+	if env_err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbUser := os.Getenv("DB_USER")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", dbHost,dbUser, dbPassword, dbName)
+
     var err error
     DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
