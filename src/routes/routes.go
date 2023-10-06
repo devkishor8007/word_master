b3470de5b8bfec4fef90f0652ec6d2bc483d7b28
devkishor <kishor8007kc@gmail.com>
2023-10-07 00:57:15 +0545

    refactor: authentication middleware along with remove redudant code

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -3,12 +3,13 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"github.com/devkishor8007/word_master/src/controllers"
+	"github.com/devkishor8007/word_master/src/middleware"
 	"net/http"
 )
 
 func SetupRoutes(r *mux.Router) {
 	r.HandleFunc("/signup", controllers.Register).Methods("POST")
 	r.HandleFunc("/signin", controllers.Login).Methods("POST")
-	r.Handle("/protected", controllers.RequiredAuth(http.HandlerFunc(controllers.Home))).Methods("GET")
+	r.Handle("/protected", middleware.RequiredAuth(http.HandlerFunc(controllers.Home))).Methods("GET")
 	r.HandleFunc("/", controllers.Home).Methods("GET")
 }
