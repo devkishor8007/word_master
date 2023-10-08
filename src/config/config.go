2598de62deee528f6d33f13a5c0d50602153fbdd
devkishor <kishor8007kc@gmail.com>
2023-10-08 23:38:51 +0545

    chore: add jwt secret environment

--- src/config/config.go
+++ src/config/config.go
@@ -6,6 +6,6 @@ import (
 )
 
 var (
-    JWTSecret   = []byte(os.Getenv("JWT_SECRET")) // Change this to your secret key
+    JWTSecret   = []byte(os.Getenv("JWT_SECRET"))
     TokenExpiry = 7 * 24 * time.Hour        // Token expiration after 7 days
 )
