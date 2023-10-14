a34a9786f67c142c4f581446bfc7322812b7301f
devkishor <kishor8007kc@gmail.com>
2023-10-14 15:43:02 +0545

    refactor(models): db relational model

--- src/models/article.model.go
+++ src/models/article.model.go
@@ -11,4 +11,5 @@ type Article struct {
 	PublicationDate time.Time `json:"publication_date"`
 	AuthorID        uint      `json:"author_id"`
 	CategoryID      uint      `json:"category_id"`
+	Category        Category  `json:"category" gorm:"foreignKey:CategoryID"`
 }
--- src/models/category.model.go
+++ src/models/category.model.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Category struct {
-	// CategoryID uint   `gorm:"primaryKey" json:"category_id"`
 	gorm.Model
 	Name string `json:"name" gorm:"unique;not null"`
+	Articles []Article
 }
